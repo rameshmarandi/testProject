dc841a5024a07c190c9b7c789be441d4b4d08de6
Ramesh Marandi <marandiramesh91@gmail.com>
2024-03-30 11:45:58 +0530

    first

---
+++ src/Component/ButtonSection.js
@@ -0,0 +1,16 @@
+import { View, Text , Button} from 'react-native'
+import React from 'react'
+
+const ButtonSection = () => {
+  return (
+    <View>
+      <Text>ButtonSection</Text>
+      <Button
+      title='Ramesh'
+      />
+      <Text>This is text compoentn here</Text>
+    </View>
+  )
+}
+
+export default ButtonSection
