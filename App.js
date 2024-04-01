51fa3979b85548ea5ae219448af67391dc9b74d1
Ramesh Marandi <marandiramesh91@gmail.com>
2024-04-01 10:55:55 +0530

    changes in app.js ifle
    changes in app.js ifle

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React,{useEffect} from 'react'
 import ButtonSection from './src/Component/ButtonSection'
+import axios from 'axios'; 
+
 
 const App = () => {
+
+  // useEffect(async()=>{
+
+  //   try {
+  //     const response = await axios.get('https://github.com/rameshmarandi/testProject/blob/main/src/Component/ButtonSection.js');
+  //     // return response.data;
+
+  //     console.log("api fetching res", response)
+  //   } catch (error) {
+  //     console.error('Error fetching component code:', error);
+  //     return null; // Handle errors gracefully (e.g., display an error message)
+  //   }
+  // },[])
   return (
     <View>
       <Text>App</Text>
